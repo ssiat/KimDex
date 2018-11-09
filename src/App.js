3637a0a53f85685a2a999c0aa1ef980fe67f727c
ahb0327 <ahb0327@naver.com>
2018-11-09 15:40:46 +0900

    Create Content.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './Header'
+import Content from './Content'
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Header/>
+                <Content/>
             </div>
         );
     }
---
+++ src/Content.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { Media } from "reactstrap";
+
+class Content extends Component {
+  render() {
+    return (
+      <Media list className="list-unstyled">
+        <Media className="my-4" tag="li">
+          <Media left>
+            <Media
+              object
+              className="mr-3"
+              data-src="holder.js/64x64"
+              alt="Generic placeholder image"
+            />
+          </Media>
+          <Media body>
+            <Media heading>Middle aligned media</Media>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+            scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum
+            in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
+            nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+          </Media>
+        </Media>
+      </Media>
+    );
+  }
+}
+
+export default Content;

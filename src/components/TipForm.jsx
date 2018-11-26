40fe92d2e5a845d73830aa23d6fdb2e6b81d0073
ahb0327 <ahb0327@naver.com>
2018-11-26 20:35:44 +0900

    Update delay time form

--- src/components/TipForm.jsx
+++ src/components/TipForm.jsx
@@ -1,18 +1,27 @@
 import React, { Component } from "react";
-import { Button, Col, Input, Row } from "reactstrap";
+import {
+  Button,
+  Col,
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  Row
+} from "reactstrap";
+import Axios from "axios";
 
 export class TipForm extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      text: ""
+      text: "",
+      delayTime: ""
     };
   }
 
   handleChange = event => {
     this.setState({
-      text: event.target.value
+      [event.target.name]: event.target.value
     });
   };
 
@@ -22,20 +31,55 @@ export class TipForm extends Component {
     this.setState({ text: "" });
   };
 
+  handleSetDelayTime = event => {
+    event.preventDefault();
+    console.log(this.state)
+    Axios.post("http://localhost:3002/setting/delaytime", {
+      time: this.state.delayTime
+    });
+  };
+
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <Row>
-          <Col>
-            <Input placeholder="새로운 팁을 입력해주세요." value={this.state.text} onChange={this.handleChange} name="text" />
-          </Col>
-          <Col className="align-items-center">
-            <Button type="submit" color="primary" onClick={this.handleCreate}>
-              생성
-            </Button>
-          </Col>
-        </Row>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <Row>
+            <Col xs="10">
+              <Input
+                placeholder="새로운 팁을 입력해주세요."
+                value={this.state.text}
+                onChange={this.handleChange}
+                name="text"
+              />
+            </Col>
+            <Col xs="2" className="align-items-center">
+              <Button type="submit" color="primary" onClick={this.handleCreate}>
+                생성
+              </Button>
+            </Col>
+          </Row>
+        </form>
+        <form>
+          <Row>
+            <Col xs="10">
+              <InputGroup>
+                <Input
+                  placeholder="지연 시간"
+                  value={this.state.delayTime}
+                  onChange={this.handleChange}
+                  name="delayTime"
+                />
+                <InputGroupAddon addonType="append">분</InputGroupAddon>
+              </InputGroup>
+            </Col>
+            <Col xs="2" className="align-items-center">
+              <Button type="submit" color="primary" onClick={this.handleSetDelayTime}>
+                적용
+              </Button>
+            </Col>
+          </Row>
+        </form>
+      </div>
     );
   }
 }
